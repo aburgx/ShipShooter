c90eeff62e82ec859d3d46f14be6a05b4d72edf9
burgi27 <alexander.burghuber@gmail.com>
2019-05-27 21:43:54 +0200

    added start/reset button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 
 @Component({
     selector: 'app-root',
-    template: '<div #canvas style="width: 100%; height: 100%"></div>',
+    templateUrl: 'app.component.html',
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, AfterViewInit {
@@ -15,67 +15,94 @@ export class AppComponent implements OnInit, AfterViewInit {
     private scene: THREE.Scene = new THREE.Scene();
     private renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({antialias: true});
 
-    private width = window.innerWidth;
-    private height = window.innerHeight;
-
     private dragging = false;
     private x: number;
     private y: number;
 
+    private blueUnits = [];
+    private redUnits = [];
+    private isRunning = false;
+
     @HostListener('document:mousedown', ['$event'])
-    onmousedown(event: MouseEvent) {
+    onmousedown(evt: MouseEvent) {
         this.dragging = true;
-        this.x = event.x;
-        this.y = event.y;
+        this.x = evt.x;
+        this.y = evt.y;
     }
 
-    @HostListener('document:mouseup', ['$event'])
-    onmouseup(event: MouseEvent) {
+    @HostListener('document:mouseup')
+    onmouseup() {
         this.dragging = false;
     }
 
     @HostListener('document:mousemove', ['$event'])
-    onmousemove(event: MouseEvent) {
+    onmousemove(evt: MouseEvent) {
         if (this.dragging) {
-            const offsetx = this.x - event.x;
-            this.x = event.x;
-            const offsety = event.y - this.y;
-            this.y = event.y;
-            this.moveCamera(offsetx, offsety);
+            const offsetX = this.x - evt.x;
+            this.x = evt.x;
+            const offsetY = evt.y - this.y;
+            this.y = evt.y;
+            this.moveCamera(offsetX, offsetY);
         }
     }
 
     ngOnInit(): void {
-        this.camera = new THREE.PerspectiveCamera(75, this.width / this.height, 0.1, 1000);
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         this.camera.position.set(0, 15, 30);
         this.camera.lookAt(this.scene.position);
         this.scene.add(new THREE.AxesHelper(10));
         this.scene.background = new THREE.Color('lightblue');
-        /*
-        const skyGeometry = new THREE.SphereGeometry(1000, 25, 25);
-        const material = new THREE.MeshLambertMaterial({color: 'white'});
-        material.side = THREE.DoubleSide;
-        const sky = new THREE.Mesh(skyGeometry, material);
-        this.scene.add(sky);
-        */
-        this.basicBox();
+        this.createEnvironment();
     }
 
     ngAfterViewInit(): void {
         this.canvas.nativeElement.appendChild(this.renderer.domElement);
-        this.renderer.setSize(this.width, this.height);
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.renderer.shadowMap.enabled = true;
         this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
         this.render();
     }
 
-    basicBox() {
-        const box = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1.5, 2),
-            new THREE.MeshPhongMaterial({color: 'blue'})
+    render() {
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    moveCamera(offsetX: number, offsetY: number) {
+        this.camera.position.applyAxisAngle(new THREE.Vector3(0, 1, 0), offsetX / 100);
+        this.camera.position.y -= offsetY;
+        this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+        this.render();
+    }
+
+    onStart() {
+        const unit1 = this.createUnit('red');
+        unit1.position.set(-15, 0, 0);
+        this.blueUnits.push(unit1);
+
+        const unit2 = this.createUnit('blue');
+        unit2.position.set(15, 0, 0);
+        this.redUnits.push(unit2);
+
+        this.isRunning = true;
+        this.blueUnits.forEach(unit => this.animate(unit, .1));
+        this.redUnits.forEach(unit => this.animate(unit, -.1));
+    }
+
+    onReset() {
+        this.isRunning = false;
+        this.blueUnits.forEach(unit => this.scene.remove(unit));
+        this.redUnits.forEach(unit => this.scene.remove(unit));
+        this.blueUnits = [];
+        this.redUnits = [];
+    }
+
+    createEnvironment() {
+        const ground = new THREE.Mesh(
+            new THREE.BoxGeometry(60, 40, 1),
+            new THREE.MeshPhongMaterial({color: 'lightgreen'})
         );
-        box.position.set(2, 2, 2);
-        box.castShadow = true;
+        ground.position.set(0, -1.5, 0);
+        ground.rotateX(-Math.PI / 2);
 
         /*
         const light = new THREE.SpotLight();
@@ -83,44 +110,45 @@ export class AppComponent implements OnInit, AfterViewInit {
         light.position.set(0, 100, 0);
         light.shadow.mapSize.width = light.shadow.mapSize.height = 10000;
         light.castShadow = true;
-         */
-
-        const plane = new THREE.Mesh(
-            new THREE.PlaneGeometry(60, 40, 1, 1),
-            new THREE.MeshPhongMaterial({color: 'lightgreen'})
-        );
-        plane.rotateX(-Math.PI / 2);
+       */
 
         const light = new THREE.DirectionalLight(0xffffff, 0.8);
-        light.target = plane;
+        light.target = ground;
         light.castShadow = true;
-        light.shadow.mapSize.width = light.shadow.mapSize.height = 10000;
+        light.shadow.mapSize.width = light.shadow.mapSize.height = 1024;
+        this.scene.add(ground, light);
+    }
 
-        this.scene.add(box, light, plane);
+    createUnit(color: string) {
+        const unit = new THREE.Mesh(
+            new THREE.BoxGeometry(2, 2, 2),
+            new THREE.MeshPhongMaterial({color})
+        );
+        unit.castShadow = true;
+        this.scene.add(unit);
+        return unit;
+    }
+
+    /*
+    animate() {
+        this.render();
+        requestAnimationFrame(() => this.animate());
+    }*/
+
+    animate(obj: THREE.Object3D, distance: number) {
+        obj.translateX(distance);
+        this.render();
+        if (this.isRunning) {
+            requestAnimationFrame(() => this.animate(obj, distance));
+        }
     }
 
     /*
     private activateDragging() {
-        const dragControls = new DragControls(this.scene, this.camera, this.renderer.domElement);
-        dragControls.
-        this.animate();
+      const dragControls = new DragControls(this.scene, this.camera, this.renderer.domElement);
+      dragControls.
+      this.animate();
     }
     */
-
-    private render() {
-        this.renderer.render(this.scene, this.camera);
-    }
-
-    private animate() {
-        this.render();
-        requestAnimationFrame(() => this.animate());
-    }
-
-    private moveCamera(offsetx: number, offsety: number) {
-        this.camera.position.applyAxisAngle(new THREE.Vector3(0, 1, 0), offsetx / 100);
-        this.camera.position.y -= offsety;
-        this.camera.lookAt(new THREE.Vector3(0, 0, 0));
-        this.render();
-    }
 
 }
